2ea30fd70e359f58555833836fdae03f697b55f4
Freewhisper77 <emmasddx@foxmail.com>
2022-11-03 15:38:12 +0800

    docs: move the code of JS in index.html to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+const inviteBtn = document.getElementById("enroll");
+inviteBtn.onclick = function (e) {
+    e.preventDefault();
+    inviteBtn.innerText = "报名成功";
+    inviteBtn.style.background = "green";
+    inviteBtn.style.borderColor = "green";
+}
